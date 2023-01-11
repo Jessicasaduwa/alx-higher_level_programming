cb51cc52e862c5faf7a508651c1b533743dcaeca
Jessica22 <113600974+Jessicasaduwa@users.noreply.github.com>
2023-01-11 02:28:14 +0100

    Import Modules in Python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,13 @@
-i
+#!/usr/bin/python3
+
+import hidden_4
+
+i = 0
+
+if __name__ == '__main__':
+    mylist = dir(hidden_4)
+    newlist = sorted(mylist)
+    while i < len(newlist):
+        if newlist[i][0] != '_':
+            print(newlist[i])
+        i += 1
